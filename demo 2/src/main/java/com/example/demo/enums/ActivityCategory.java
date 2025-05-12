4bf7aa594c153a9be82b266d7e5c017e8a8cbff7
Lucia <luciacastagninoo@gmail.com>
2025-05-12 11:58:53 -0300

    Enums

---
+++ demo 2/src/main/java/com/example/demo/enums/ActivityCategory.java
@@ -0,0 +1,14 @@
+package com.example.demo.enums;
+
+public enum ActivityCategory {
+    ADVENTURE,
+    CULTURAL,
+    RELAX,
+    GASTRONOMY,
+    NATURE,
+    NIGHTLIFE,
+    SHOPPING,
+    SPORTS,
+    HISTORY,
+    FAMILY
+}
---
+++ demo 2/src/main/java/com/example/demo/enums/ExpenseCategory.java
@@ -0,0 +1,15 @@
+package com.example.demo.enums;
+
+public enum ExpenseCategory {
+    TRANSPORTATION,
+    ACCOMMODATION,
+    FOOD,
+    ACTIVITIES,
+    SOUVENIRS,
+    TICKETS,
+    UNEXPECTED,
+    HEALTH,
+    COMMUNNICATION, //referencia a tarjetas sim, wifi, etc
+    OTHER
+
+}
