c33f438df775a52654407e44799930a34b591f5a
Brenda2310 <brendafernandez2310@gmail.com>
2025-05-12 11:18:41 -0300

    New entities

---
+++ demo 2/src/main/java/com/example/demo/entities/ItineraryEntity.java
@@ -0,0 +1,13 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ItineraryEntity {
+}
