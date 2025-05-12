b841ed67440a8023ad4ab7b5ef3b8b9f50e32693
Brenda2310 <brendafernandez2310@gmail.com>
2025-05-12 12:02:48 -0300

    Change

--- demo 2/src/main/java/com/example/demo/services/TripService.java
+++ demo 2/src/main/java/com/example/demo/services/TripService.java
@@ -1,9 +1,12 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.TripEntity;
 import com.example.demo.repositories.ActivityRepository;
 import com.example.demo.repositories.TripRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TripService {
@@ -13,4 +16,10 @@ public class TripService {
     public TripService(TripRepository tripRepository) {
         this.tripRepository = tripRepository;
     }
+
+    public List<TripEntity> findAll(){
+        return tripRepository.findAll();
+    }
+
+
 }
--- demo 2/src/main/java/com/example/demo/services/UserService.java
+++ demo 2/src/main/java/com/example/demo/services/UserService.java
@@ -12,4 +12,6 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+
 }
