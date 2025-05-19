08ae59b8434a41a68ace00f93c343f8ff4c60c3c
Lucia <luciacastagninoo@gmail.com>
2025-05-19 12:00:40 -0300

    EnvironmentConfig

---
+++ demo 2/src/main/java/com/example/demo/config/EnvironmentCofig.java
@@ -0,0 +1,18 @@
+package com.example.demo.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
+
+@Configuration
+public class EnvironmentCofig {
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("api_key", Objects.requireNonNull(dotenv.get("API_KEY")));
+        System.setProperty("db_url", Objects.requireNonNull(dotenv.get("DB_URL")));
+        System.setProperty("db_user", Objects.requireNonNull(dotenv.get("DB_USER")));
+        System.setProperty("db_password", Objects.requireNonNull(dotenv.get("DB_PASSWORD")));
+    }
+}
