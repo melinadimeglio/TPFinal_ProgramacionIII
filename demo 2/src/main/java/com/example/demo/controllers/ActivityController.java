3f2c7787d3f1f24808640d54972039ae321409c3
Melina Di Meglio <meli.dime@gmail.com>
2025-05-19 09:27:12 -0300

    changes JPARepository

--- demo 2/src/main/java/com/example/demo/controllers/ActivityController.java
+++ demo 2/src/main/java/com/example/demo/controllers/ActivityController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/activities")
-public class ActivityController {
+public class ActivityController{
 
     private final ActivityService activityService;
 
--- demo 2/src/main/java/com/example/demo/repositories/CredentialRepository.java
+++ demo 2/src/main/java/com/example/demo/repositories/CredentialRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repositories;
 
 import com.example.demo.entities.CheckListEntity;
+import com.example.demo.entities.CredentialEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CredentialRepository extends JpaRepository <CheckListEntity, Long> {
+public interface CredentialRepository extends JpaRepository <CredentialEntity, Long> {
 }
