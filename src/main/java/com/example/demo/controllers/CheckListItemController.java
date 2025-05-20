3686c9ac97d8aebba435896e75ee73675140f784
Lucia <luciacastagninoo@gmail.com>
2025-05-20 17:15:13 -0300

    Change

--- src/main/java/com/example/demo/controllers/CheckListItemController.java
+++ src/main/java/com/example/demo/controllers/CheckListItemController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "CheckList", description = "Operations related to users' checklist items")
+@Tag(name = "CheckListItem", description = "Operations related to users' checklist items")
 @RestController
 @RequestMapping("/checklistsItem")
 public class CheckListItemController {
