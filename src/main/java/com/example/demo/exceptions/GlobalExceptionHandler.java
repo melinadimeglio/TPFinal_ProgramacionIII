f02a97b5097737f7f654b42123280e9279a0a5b6
Brenda2310 <brendafernandez2310@gmail.com>
2025-05-29 16:20:27 -0300

    Exceptions Changes

--- src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/GlobalExceptionHandler.java
@@ -1,16 +1,19 @@
 package com.example.demo.exceptions;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -49,6 +52,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleRepitedElementExceptions (RepitedElementException e){
         String message = e.getMessage() != null ? e.getMessage() : "El elemento ya existe y debe ser único.";
         return ResponseEntity.status(HttpStatus.CONFLICT).body(message);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        String error = e.getMessage() != null ? e.getMessage() : "El recurso no fue encontrado.";
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
 }
