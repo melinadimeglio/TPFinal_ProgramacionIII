4144a20567eeab590171892eb4a9272c1879d9ad
Melina Di Meglio <meli.dime@gmail.com>
2025-06-11 19:02:48 -0300

    update userMapper

--- src/main/java/com/example/demo/mappers/UserMapper.java
+++ src/main/java/com/example/demo/mappers/UserMapper.java
@@ -5,6 +5,7 @@ import com.example.demo.DTOs.User.Response.UserResponseDTO;
 import com.example.demo.DTOs.User.UserUpdateDTO;
 import com.example.demo.entities.UserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
@@ -14,6 +15,7 @@ import java.util.List;
 nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
+    @Mapping(source = "trips", target = "destinos")
     UserResponseDTO toDTO(UserEntity user);
     UserEntity toUserEntity (UserCreateDTO userDTO);
 
