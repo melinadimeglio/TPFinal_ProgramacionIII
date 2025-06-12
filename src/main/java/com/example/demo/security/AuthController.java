b313d80940d6a1f3ab45ec936ec827ad69c657d0
Melina Di Meglio <meli.dime@gmail.com>
2025-06-12 19:44:44 -0300

    update AuthController

--- src/main/java/com/example/demo/security/AuthController.java
+++ src/main/java/com/example/demo/security/AuthController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Authentication", description = "Operations related to authentication, token management and user sessions")
 public class AuthController {
 
     private final AuthService authService;
