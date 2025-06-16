f9194664e76606a70d19de9b6eadecfd368868d3
Melina Di Meglio <meli.dime@gmail.com>
2025-06-16 19:18:38 -0300

    update itineraries documentation

--- src/main/java/com/example/demo/DTOs/Filter/ItineraryFilterDTO.java
+++ src/main/java/com/example/demo/DTOs/Filter/ItineraryFilterDTO.java
@@ -2,12 +2,14 @@ package com.example.demo.DTOs.Filter;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+import org.springdoc.core.annotations.ParameterObject;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ParameterObject
 public class ItineraryFilterDTO {
 
     @Schema(description = "Fecha inicial para filtrar (formato yyyy-MM-dd)", example = "2025-06-01")
@@ -15,5 +17,4 @@ public class ItineraryFilterDTO {
 
     @Schema(description = "Fecha final para filtrar (formato yyyy-MM-dd)", example = "2025-06-30")
     private String dateTo;
-
  }
