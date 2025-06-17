495e213c910a20accf2d0a67fc0e5d96e9f3df28
Melina Di Meglio <meli.dime@gmail.com>
2025-06-16 22:27:21 -0300

    update UserUpdateDTO

--- src/main/java/com/example/demo/DTOs/User/UserUpdateDTO.java
+++ src/main/java/com/example/demo/DTOs/User/UserUpdateDTO.java
@@ -24,18 +24,15 @@ import java.util.Set;
 public class UserUpdateDTO {
 
     @Schema(description = "Nombre de usuario", example = "melinaD")
-    @NotBlank(message = "El nombre de usuario no debe estar vacio.")
     @Column(unique = true)
     @Size(min = 5, max = 20, message = "El username debe tener entre 5 y 20 caracteres.")
     private String username;
 
     @Schema(description = "Email del usuario", example = "melina@example.com")
     @Email(message = "El email debe tener un formato válido.")
-    @NotBlank(message = "El email no debe estar vacio.")
     private String email;
 
     @Schema(description = "Contraseña del usuario", example = "Progra3_2025")
-    @NotBlank(message = "La contraseña no debe estar vacia.")
     @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&*!()_+=\\-]).{8,20}$",
             message = "La contraseña debe tener entre 8 y 20 caracteres, incluyendo una mayúscula, una minúscula, un número y un carácter especial."
@@ -43,7 +40,6 @@ public class UserUpdateDTO {
     private String password;
 
     @Schema(description = "DNI del usuario", example = "40123456")
-    @NotBlank(message = "El DNI no debe estar vacio.")
     @Pattern(regexp = "^[0-9]{7,8}$", message = "El DNI debe tener entre 7 y 8 dígitos")
     private String dni;
 
